24ccea0ee53b8fd92f91fa1f9dc9eb371ba45214
Shahil Ahmed <shahil@agpl.net>
2025-03-06 15:36:55 +0530

    Route Returning String

--- ProjectTwo/routes/web.php
+++ ProjectTwo/routes/web.php
@@ -14,5 +14,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// Route::get('contact', function(){
+//     return view('contact');
+// });
+
+Route::get('contact',function(){
+    return view('contactme',['name'=>'Shahil']);
+});
+
+Route::get('adminprofile',function(){
+    return view('admin.profile');
+});
+
+
+Route::get('about', function () {
+    return view('about');
+});
+
+// Route Returning String
+// Route::view('about','about');
+
+// Route::get('admin',function () {
+//     return view('profile');
+// });
