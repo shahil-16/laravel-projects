b1b1775d5f82de94c3479fe6bb960507d95ddc88
Shahil Ahmed <shahil@agpl.net>
2025-03-12 11:48:26 +0530

    Admin Page added

--- ProjectThree/routes/web.php
+++ ProjectThree/routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('user/register', [UserController::class, 'register'])->name('user-register');
+Route::get('user/login',[UserController::class,'login'])->name('user-login');
+Route::get('admin/login',[AdminController::class,'login'])->name('admin-login');
+Route::get('admin/dashboard',[AdminController::class,'dashboard'])->name('admin-dashboard');
